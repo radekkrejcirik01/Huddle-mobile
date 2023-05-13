025636e5014b4319cca3199869eb442475ac1c1e
radekkrejcirik <radekkrejcirik@Radeks-MacBook-Air.local>
2023-05-13 09:56:42 +0200

    feat: useRenderNotifications

--- src/screens/account/NotificationsScreen/NotificationsScreen.tsx
+++ src/screens/account/NotificationsScreen/NotificationsScreen.tsx
@@ -1,43 +1,19 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { RefreshControl, View } from 'react-native';
+import { View } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
-import { FlashList, ListRenderItemInfo } from '@shopify/flash-list';
+import { FlashList } from '@shopify/flash-list';
 import { ReducerProps } from '@store/index/index.props';
-import { useNavigation } from '@hooks/useNavigation';
-import { useRefresh } from '@hooks/useRefresh';
-import { useOpenProfilePhoto } from '@hooks/useOpenProfilePhoto';
-import { useOpenChat } from '@hooks/useOpenChat';
-import { RootStackNavigatorEnum } from '@navigation/RootNavigator/RootStackNavigator.enum';
 import { NotificationsScreenStyle } from '@screens/account/NotificationsScreen/NotificationsScreen.style';
 import { NotificationsListProps } from '@screens/account/NotificationsScreen/NotificationsScreen.props';
-import {
-    getRequestUser,
-    postRequestUser,
-    putRequestUser
-} from '@utils/Axios/Axios.service';
-import {
-    ResponseInterface,
-    ResponseNotificationsGetInterface,
-    ResponsePeopleNumberGetInterface
-} from '@interfaces/response/Response.interface';
-import {
-    AcceptPersonInviteInterface,
-    HuddleConfirmPostInterface
-} from '@interfaces/post/Post.inteface';
-import { NotificationListItem } from '@components/notifications/NotificationListItem/NotificationListItem';
+import { getRequestUser } from '@utils/Axios/Axios.service';
+import { ResponseNotificationsGetInterface } from '@interfaces/response/Response.interface';
 import { ItemSeparator } from '@components/general/ItemSeparator/ItemSeparator';
-import { AccountStackNavigatorEnum } from '@navigation/StackNavigators/account/AccountStackNavigator.enum';
-import {
-    setNotificationsNumberAction,
-    setPeopleNumberAction
-} from '@store/UserReducer';
+import { setNotificationsNumberAction } from '@store/UserReducer';
+import { useRenderNotifications } from '@hooks/useRenderNotifications';
 
 export const NotificationsScreen = (): JSX.Element => {
     const { username } = useSelector((state: ReducerProps) => state.user.user);
     const dispatch = useDispatch();
-
-    const openProfilePhoto = useOpenProfilePhoto();
-    const openChat = useOpenChat();
 
     const [data, setData] = useState<Array<NotificationsListProps>>([]);
 
@@ -51,114 +27,23 @@ export const NotificationsScreen = (): JSX.Element => {
         });
     }, [username]);
 
-    const getPeopleNumber = useCallback(() => {
-        getRequestUser<ResponsePeopleNumberGetInterface>(
-            `people-number/${username}`
-        ).subscribe((response: ResponsePeopleNumberGetInterface) => {
-            if (response?.status) {
-                dispatch(setPeopleNumberAction(response?.peopleNumber));
-            }
-        });
-    }, [dispatch, username]);
-
     useEffect(() => {
         dispatch(setNotificationsNumberAction(0));
 
         return () => {};
     }, [dispatch]);
 
-    const { navigateTo } = useNavigation(
-        RootStackNavigatorEnum.AccountStack,
-        loadNotifications
-    );
-
-    const { refreshing, onRefresh } = useRefresh(loadNotifications);
-
-    const acceptPersonInvite = useCallback(
-        (item: NotificationsListProps) => {
-            putRequestUser<ResponseInterface, AcceptPersonInviteInterface>(
-                'person',
-                {
-                    id: item?.eventId,
-                    sender: username,
-                    receiver: item?.sender
-                }
-            ).subscribe((response) => {
-                if (response?.status) {
-                    loadNotifications();
-                    getPeopleNumber();
-                }
-            });
-        },
-        [getPeopleNumber, loadNotifications, username]
-    );
-
-    const openHuddleFromNotification = useCallback(
-        (item: NotificationsListProps) => {
-            navigateTo(AccountStackNavigatorEnum.HuddleScreen, {
-                huddleId: item?.eventId
-            });
-        },
-        [navigateTo]
-    );
-
-    const confirmHuddle = useCallback(
-        (item: NotificationsListProps) => {
-            postRequestUser<ResponseInterface, HuddleConfirmPostInterface>(
-                'huddle/confirm',
-                {
-                    huddleId: item?.eventId,
-                    sender: username,
-                    receiver: item?.sender
-                }
-            ).subscribe((response: ResponseInterface) => {
-                if (response?.status) {
-                    loadNotifications();
-                }
-            });
-        },
-        [loadNotifications, username]
-    );
-
-    const renderItem = useCallback(
-        ({ item }: ListRenderItemInfo<NotificationsListProps>): JSX.Element => (
-            <NotificationListItem
-                listItem={item}
-                onOpenAccount={() =>
-                    openProfilePhoto(item.senderName, item?.profilePhoto)
-                }
-                onAcceptPersonInvite={() => acceptPersonInvite(item)}
-                onOpenChat={() =>
-                    openChat(item.senderName, item?.profilePhoto, item?.sender)
-                }
-                onOpenHuddle={() => openHuddleFromNotification(item)}
-                onConfirmHuddle={() => confirmHuddle(item)}
-            />
-        ),
-        [
-            acceptPersonInvite,
-            confirmHuddle,
-            openChat,
-            openHuddleFromNotification,
-            openProfilePhoto
-        ]
-    );
+    const { renderNotificationItem, refreshControl, keyNotificationExtractor } =
+        useRenderNotifications(loadNotifications);
 
     return (
         <View style={NotificationsScreenStyle.container}>
             <FlashList
                 data={data}
-                refreshControl={
-                    <RefreshControl
-                        refreshing={refreshing}
-                        onRefresh={onRefresh}
-                        tintColor="white"
-                    />
-                }
-                renderItem={renderItem}
-                keyExtractor={(item: NotificationsListProps) =>
-                    item?.id?.toString()
-                }
+                extraData={data}
+                renderItem={renderNotificationItem}
+                refreshControl={refreshControl}
+                keyExtractor={keyNotificationExtractor}
                 estimatedItemSize={68}
                 showsVerticalScrollIndicator={false}
                 ItemSeparatorComponent={() => <ItemSeparator space={25} />}
