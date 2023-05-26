7f08071a40640653d063461a1e0e98a549b2fa28
radekkrejcirik <radekkrejcirik@Radeks-MacBook-Air.local>
2023-05-26 17:14:35 +0200

    refactor: remove navigation onReady callback

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,32 +1,14 @@
-import React, { useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import SplashScreen from 'react-native-splash-screen';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { RootStackNavigator } from '@navigation/RootNavigator/RootStackNavigator';
 import { DARK_THEME } from '@navigation/navigation.const';
-import { ReducerProps } from '@store/index/index.props';
-import { setIsReady } from '@store/NavigationState';
 import { NavigationService } from '@utils/general/NavigationService';
 
-export const Navigation = (): JSX.Element => {
-    const { username } = useSelector((state: ReducerProps) => state.user.user);
-    const dispatch = useDispatch();
-
-    const onReady = useCallback(() => {
-        dispatch(setIsReady(true));
-
-        if (!username) {
-            SplashScreen.hide();
-        }
-    }, [dispatch, username]);
-
-    return (
-        <NavigationContainer
-            ref={(ref) => NavigationService.setNavigationRef(ref)}
-            onReady={onReady}
-            theme={DARK_THEME}
-        >
-            <RootStackNavigator />
-        </NavigationContainer>
-    );
-};
+export const Navigation = (): JSX.Element => (
+    <NavigationContainer
+        ref={(ref) => NavigationService.setNavigationRef(ref)}
+        theme={DARK_THEME}
+    >
+        <RootStackNavigator />
+    </NavigationContainer>
+);
