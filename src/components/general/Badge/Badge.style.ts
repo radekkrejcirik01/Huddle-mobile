3019c32d0c778e17c1551b10db8432761d5ee787
radekkrejcirik <radekkrejcirik@Radeks-MacBook-Air.local>
2023-07-29 13:13:44 +0200

    feat: badge color change

--- src/components/general/Badge/Badge.style.ts
+++ src/components/general/Badge/Badge.style.ts
@@ -9,7 +9,7 @@ export const BadgeStyle = StyleSheet.create({
         paddingHorizontal: 5,
         paddingBottom: 2,
         borderRadius: 10,
-        backgroundColor: COLORS.RED,
+        backgroundColor: COLORS.PASTEL_RED,
         position: 'absolute',
         alignSelf: 'flex-end'
     },
